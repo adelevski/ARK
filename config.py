080c4313c23e068c8dd54879b1c116441d6fb352
Atanas Delevski <adelevski@gmail.com>
2021-03-31 14:52:05 -0500

    next update

---
+++ config.py
@@ -0,0 +1,5 @@
+API_URL = 'https://paper-api.alpaca.markets'
+
+API_KEY = 'PKO4H38ZTWYRZNSJTA4Y'
+
+API_SECRET = '0nX8A0ATvqw1nssYdYzpiM5LYN6YjycDoomBWmYc'
--- db/create_tables.sql
+++ db/create_tables.sql
@@ -3,16 +3,36 @@ CREATE TABLE stock (
     symbol TEXT NOT NULL,
     name TEXT NOT NULL,
     exchange TEXT NOT NULL,
-    is_etf BOOLEAN NOT NULL,
+    is_etf BOOLEAN NOT NULL
 );
 
+-- 1, 3 - 1 ARKG, CRSP
+-- 1, 4 - 1 ARKG, EDIT
+-- 1, 5 - 1 ARKG, TDOC
 CREATE TABLE etf_holding(
     etf_id INTEGER NOT NULL,
     holding_id INTEGER NOT NULL,
     dt DATE NOT NULL,
-    shares NUMERIC
+    shares NUMERIC,
+    weight NUMERIC,
+    PRIMARY KEY(etf_id, holding_id, dt),
+    CONSTRAINT fk_etf FOREIGN KEY (etf_id) REFERENCES stock (id),
+    CONSTRAINT fk_holding FOREIGN KEY (holding_id) REFERENCES stock (id)
 );
 
 CREATE TABLE stock_price (
-
+    stock_id INTEGER NOT NULL,
+    dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    open NUMERIC NOT NULL,
+    high NUMERIC NOT NULL,
+    low NUMERIC NOT NULL,
+    close NUMERIC NOT NULL,
+    volume NUMERIC NOT NULL,
+    PRIMARY KEY (stock_id, dt),
+    CONSTRAINT fk_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
 );
+
+CREATE INDEX ON stock_price (stock_id, dt DESC);
+
+SELECT create_hypertable('stock_price', 'dt');
+
