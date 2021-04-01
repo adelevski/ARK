efd1c1711c574d7ffb7e4cce4a489da30ffbf750
Atanas Delevski <adelevski@gmail.com>
2021-03-31 19:45:22 -0500

    finishing firsst script

--- populate_db.py
+++ populate_db.py
@@ -1,12 +1,10 @@
 import config
 import alpaca_trade_api as tradeapi
 import psycopg2
+import psycopg2.extras
 
 connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
 
-cursor = connection.cursor()
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-cursor.execute("SELECT * FROM stock")
-
-print(cursor.fetchall())
-
+api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url=config.API_URL)
