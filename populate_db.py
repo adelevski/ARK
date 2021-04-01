4e79f3a915fc69008b1321f9cb88d485680864ce
Atanas Delevski <adelevski@gmail.com>
2021-03-31 20:01:25 -0500

    finished vid

--- populate_db.py
+++ populate_db.py
@@ -8,3 +8,14 @@ connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url=config.API_URL)
+
+assets = api.list_assets()
+
+for asset in assets:
+    print(f"Inserting stock {asset.symbol}")
+    cursor.execute("""
+        INSERT INTO stock (name, symbol, exchange, is_etf)
+        VALUES (%s, %s, %s, false)
+    """, (asset.name, asset.symbol, asset.exchange))
+
+connection.commit()
