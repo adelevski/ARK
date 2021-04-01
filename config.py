58e78c79ee7d97d973472dc0c48cdecd219391a3
Atanas Delevski <adelevski@gmail.com>
2021-03-31 19:09:26 -0500

    update again

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 API_URL = 'https://paper-api.alpaca.markets'
-
 API_KEY = 'PKO4H38ZTWYRZNSJTA4Y'
-
 API_SECRET = '0nX8A0ATvqw1nssYdYzpiM5LYN6YjycDoomBWmYc'
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
+
--- populate_db.py
+++ populate_db.py
@@ -1,2 +1,12 @@
 import config
+import alpaca_trade_api as tradeapi
+import psycopg2
+
+connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM stock")
+
+print(cursor.fetchall())
 
