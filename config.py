6011d054cecf99bc0e3350197b93892e9771bd86
Atanas Delevski <adelevski@gmail.com>
2021-04-12 00:26:47 -0500

    updated api keys, added price fetching script. However, alpaca doesnt use polygon anymore, will need to find alternative source

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_URL = 'https://paper-api.alpaca.markets'
-API_KEY = 'PKO4H38ZTWYRZNSJTA4Y'
-API_SECRET = '0nX8A0ATvqw1nssYdYzpiM5LYN6YjycDoomBWmYc'
+API_URL = 'https://api.alpaca.markets'
+API_KEY = 'AKIBZWBUG1VN1YR94D52'
+API_SECRET = 'jpBc8GmajHmD9Qha99SqPUnV3K0097EqEsDQyciS'
 
 DB_HOST = 'localhost'
 DB_USER = 'postgres'
