dc57233d0697cf69d1394a7ae4497186009e4feb
Atanas Delevski <adelevski@gmail.com>
2021-05-17 20:48:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-API_URL = 'https://api.alpaca.markets'
-API_KEY = 'AKIBZWBUG1VN1YR94D52'
-API_SECRET = 'jpBc8GmajHmD9Qha99SqPUnV3K0097EqEsDQyciS'
+# API_URL = 'https://api.alpaca.markets'
+# API_KEY = 'AKIBZWBUG1VN1YR94D52'
+# API_SECRET = 'jpBc8GmajHmD9Qha99SqPUnV3K0097EqEsDQyciS'
+
+API_KEY = 'f99209dcab8a4456a3f275474bbe98b3'
 
 DB_HOST = 'localhost'
 DB_USER = 'postgres'
